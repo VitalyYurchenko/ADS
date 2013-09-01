bef5ec74b13058783499ce5de4321e7a2a625b25
Vitaly Yurchenko <Y.Vitaly@mac.com>
2013-09-01 15:52:06 +0300

    Added following example: - Binary Search: 	- Spell Checking;

---
+++ ADS/Examples/Binary Search/Spell Checking/spell.c
@@ -0,0 +1,58 @@
+//
+//  spell.c
+//  ADS
+//
+//  Created by Vitaly Yurchenko on 9/1/13.
+//  Copyright (c) 2013 Vitaly Yurchenko. All rights reserved.
+//
+// ********************************************************************************************************************************************************** //
+
+#include "spell.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "bisearch.h"
+
+// ********************************************************************************************************************************************************** //
+
+static int compare_str(const void *str1, const void *str2);
+
+// ********************************************************************************************************************************************************** //
+
+#pragma mark -
+#pragma mark Public
+
+int spell(char (*dictionary)[SPELL_SIZE], int size, const char *word)
+{
+    /* Look up the word. */
+    if (bisearch(dictionary, (void *)word, size, SPELL_SIZE, compare_str) >= 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+#pragma mark -
+#pragma mark Private
+
+static int compare_str(const void *str1, const void *str2)
+{
+    int retval;
+    
+    if ((retval = strcmp((const char *)str1, (const char *)str2)) > 0)
+    {
+        return 1;
+    }
+    else if (retval < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
