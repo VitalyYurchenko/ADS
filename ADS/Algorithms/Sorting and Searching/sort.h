86b3ab3e8bdc36a0e1e6f9c6e88f7ace4c37942f
Vitaly Yurchenko <Y.Vitaly@mac.com>
2013-09-11 11:27:16 +0300

    Added following algorithms: - Counting Sort; - Merge Sort; - Radix Sort;

--- ADS/Algorithms/Sorting and Searching/sort.h
+++ ADS/Algorithms/Sorting and Searching/sort.h
@@ -15,6 +15,16 @@
 /* Public Interface. */
 
 /*
+ Description:   Uses counting sort to sort the array of integers in data. The number of integers in data is specified by size.
+                The argument k specifies the maximum integer in data, plus 1. When ctsort returns, data contains the sorted integers.
+ 
+ Return Value:  0 if sorting is successful, or –1 otherwise.
+ 
+ Complexity:    O(n + k), where n is the number of integers to be sorted and k is the maximum integer in the unsorted set, plus 1.
+ */
+int ctsort(int *data, int size, int k);
+
+/*
  Return Value:  0 if sorting is successful, or –1 otherwise.
  
  Description:   Uses insertion sort to sort the array of elements in data. The number of elements in data is specified by size.
@@ -27,6 +37,18 @@
 int issort(void *data, int size, int esize, int (*compare)(const void *key1, const void *key2));
 
 /*
+ Return Value:  0 if sorting is successful, or –1 otherwise.
+ 
+ Description:   Uses merge sort to sort the array of elements in data. The number of elements in data is specified by size.
+                The size of each element is specified by esize. The arguments i and k define the current division being sorted
+                and initially should be 0 and size – 1, respectively. The function pointer compare specifies a user-defined function to compare elements.
+                It should perform in a manner similar to that described for issort. When mgsort returns, data contains the sorted elements.
+ 
+ Complexity:    O(n lg n), where n is the number of elements to be sorted.
+ */
+int mgsort(void *data, int size, int esize, int i, int k, int (*compare)(const void *key1, const void *key2));
+
+/*
  Description:   Uses quicksort to sort the array of elements in data. The number of elements in data is specified by size.
                 The size of each element is specified by esize. The arguments i and k define the current partition being sorted
                 and initially should be 0 and size – 1, respectively. The function pointer compare specifies a user-defined function to compare elements.
@@ -37,6 +59,17 @@ int issort(void *data, int size, int esize, int (*compare)(const void *key1, con
  Complexity:    O(n lg n), where n is the number of elements to be sorted.
  */
 int qksort(void *data, int size, int esize, int i, int k, int (*compare)(const void *key1, const void *key2));
+
+/*
+ Description:   Uses radix sort to sort the array of integers in data. The number of integers in data is specified by size.
+                The argument p specifies the number of digit positions in each integer. The argument k specifies the radix.
+                When rxsort returns, data contains the sorted integers.
+ 
+ Return Value:  0 if sorting is successful, or –1 otherwise.
+ 
+ Complexity:    O(pn + pk), where n is the number of integers to be sorted, k is the radix, and p is the number of digit positions.
+ */
+int rxsort(int *data, int size, int p, int k);
 
 // ********************************************************************************************************************************************************** //
 
